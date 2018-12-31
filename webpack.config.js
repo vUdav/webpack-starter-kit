f6bf24f69408a3d5ff4bf1f84196707b93d003d4
Валера Любимов <v.udav@bk.ru>
2018-12-31 03:52:51 +0300

    feat: add sass to webpack
    With postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  entry: ["./src/js/index.js"],
+module.exports = (env, options) => ({
+  entry: ["./src/js/index.js", "./src/scss/style.scss"],
 
   output: {
     filename: "./js/bundle.js"
@@ -20,7 +21,50 @@ module.exports = {
             presets: ["@babel/preset-env"]
           }
         }
+      },
+
+      {
+        test: /\.(sass|scss)$/,
+        include: path.resolve(__dirname, "src/scss"),
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+                url: false
+              }
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: true,
+                ident: "postcss",
+                plugins: () => {
+                  let plugins = [require("postcss-preset-env")()];
+                  if (options.mode === "production") {
+                    plugins = [...plugins, require("cssnano")()];
+                  }
+                  return plugins;
+                }
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
       }
     ]
-  }
-};
+  },
+
+  plugins: [
+    new ExtractTextPlugin({
+      filename: "./css/style.bundle.css",
+      allChunks: true
+    })
+  ]
+});
